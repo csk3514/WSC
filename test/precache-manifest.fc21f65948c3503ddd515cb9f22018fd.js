aaa9ea5807974e1ce5d6d1c1b0c3466cc81ca8ab
Jason Law <jason.law@kadensacapital.com>
2018-11-01 19:06:55 +0800

    Updates

---
+++ test/precache-manifest.fc21f65948c3503ddd515cb9f22018fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a1ad63d3f8fa36901f3",
+    "url": "/whatsapp-stickers-convert/static/css/main.d16686c7.chunk.css"
+  },
+  {
+    "revision": "7a1ad63d3f8fa36901f3",
+    "url": "/whatsapp-stickers-convert/static/js/main.7a1ad63d.chunk.js"
+  },
+  {
+    "revision": "c9864e457f03c37fb161",
+    "url": "/whatsapp-stickers-convert/static/css/1.7a389171.chunk.css"
+  },
+  {
+    "revision": "c9864e457f03c37fb161",
+    "url": "/whatsapp-stickers-convert/static/js/1.c9864e45.chunk.js"
+  },
+  {
+    "revision": "104fcc453926d6e41a77",
+    "url": "/whatsapp-stickers-convert/static/js/runtime~main.104fcc45.js"
+  },
+  {
+    "revision": "b52d4db79d83af3a9b1fb1e8ae608d35",
+    "url": "/whatsapp-stickers-convert/index.html"
+  }
+];
